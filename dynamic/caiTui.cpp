864600cc5ddaa4e406978b48314d7bef4e2a6f5e
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2021-10-03 00:02:14 +0700

    Cai tui descretion

--- dynamic/caiTui.cpp
+++ dynamic/caiTui.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int F[1005][1005], N, V, A[1005], C[1005];
 
+/*
+    TH1: chọn i đồ vật giá trị còn lại ==> F[i - 1][j - A[i]] + C[i] 
+    TH2: Không chọn đồ vật ==> F[i - 1][j]
+*/
+
 int resolve() {
     int i, j;
     for (i = 0; i <= N; i++) {
@@ -31,11 +36,11 @@ int main() {
     while (t--) {
         cin >> N >> V;
         for (i = 1; i <= N; i++) {
-            cin >> A[i];
+            cin >> A[i]; // thể tích
         }
 
         for (i = 1; i <= N; i++) {
-            cin >> C[i];
+            cin >> C[i]; // giá trị
         }
 
         cout << resolve() << endl;
