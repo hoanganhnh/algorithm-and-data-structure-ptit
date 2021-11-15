870afd167c6b8f716cd9ef955f7a81f3a2c0642a
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2021-11-15 23:39:33 +0700

    selection sort

---
+++ sort & search/sort collections/selectionSort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void selection_sort (int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+void output(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    selection_sort(a, n);
+    output(a, n);
+}
